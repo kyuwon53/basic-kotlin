db8f5a362bf22ebfb1837ba2d798fcf96a5ccf4a
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-09-26 11:02:02 +0900

    인터페이스

--- src/main/kotlin/Dog.kt
+++ src/main/kotlin/Dog.kt
@@ -2,20 +2,32 @@ fun main() {
 
 }
 
+/*
+* 인터페이스 선언 후 , 구현 클래스에 `,` 찍고 사용해주면 된다
+* */
+interface Drawable {
+    fun draw()
+}
+
 abstract class Animal {
     open fun move() {
         print("이동")
     }
 }
+
 /*
 * 코틀린은 모든게 닫혀있음
 * 상속을 받아도 닫혀있음
 * 오버라이드 하려면 open 키워드를 사용해야한다.
 * 기본 클래스를 상속 받으려면 open 키워드를 입력해야한다
 * */
-class Dog : Animal(){
+class Dog : Animal(), Drawable {
     override fun move() {
         println("우다다")
+    }
+
+    override fun draw() {
+        TODO("Not yet implemented")
     }
 }
 
